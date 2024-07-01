cf28e5cd9f40d6eb489c6b79a3c73033dbcc7a87
eduardovrs <edu.vinirodrigues@gmail.com>
2024-07-01 15:51:17 -0300

    🔧 config: add nativewind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './<custom-folder>/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6A00FF',
+        'primary-200': '#a64aff',
+        'primary-300': '#ffb1ff',
+        secondary: '#00E5FF',
+        'secondary-200': '#00829b',
+        text: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
